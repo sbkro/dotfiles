ec059c0ba447210c27c691d7b7b724bb2d83b2c0
vagrant <vagrant@osx-10_9.vagrantup.com>
2015-04-18 21:28:27 +0900

    implement tmux setup script.

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -1,3 +1,24 @@
 #!/bin/sh
 
-echo "sorry, not implemented."
+src_file=$(cd `dirname $01`;pwd)/_tmux.conf
+dst_file=~/.tmux.conf
+
+if [ -e ${dst_file} ]; then
+    rm ${dst_file}
+fi
+
+ln -s ${src_file} ${dst_file}
+
+
+echo ----------------------------------------------
+echo Please install tmux from package manager.
+echo 
+echo OSX:
+echo   $ brew install tmux
+echo 
+echo Linux:
+echo   $ yum install yum
+echo
+echo If you need local settings, please create
+echo '~/.tmux.conf_local'.
+echo ----------------------------------------------
