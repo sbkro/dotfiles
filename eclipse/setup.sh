61cdfc79df4b3c2728c4ae34024ca639aea3e773
vagrant <vagrant@osx-10_9.vagrantup.com>
2015-04-18 21:14:43 +0900

    implement eclipse setup script.

--- eclipse/setup.sh
+++ eclipse/setup.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-echo 'sorry, not implemented.'
+src_file=$(cd `dirname $01`;pwd)/_vrapperrc
+dst_file=~/.vrapperrc
+
+if [ -e ${dst_file} ]; then
+    rm ${dst_file}
+fi
+
+ln -s ${src_file} ${dst_file}
