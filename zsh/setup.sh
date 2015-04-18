a45362301776dcb34499390930b2d6776247a26d
vagrant <vagrant@osx-10_9.vagrantup.com>
2015-04-18 22:23:56 +0900

    implement zsh setup script.

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -1,3 +1,24 @@
 #! /bin/sh
 
-echo "sorry, not implemented."
+# setup oh-my-zsh
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+
+src_file=$(cd `dirname $01`;pwd)/_zshrc
+dst_file=~/.zshrc
+
+if [ -e ${dst_file} ]; then
+    rm ${dst_file}
+fi
+
+ln -s ${src_file} ${dst_file}
+
+
+src_file=$(cd `dirname $01`;pwd)/sbkro
+dst_file=~/.oh-my-zsh/custom/plugins/sbkro
+
+if [ -e ${dst_file} ]; then
+    rm ${dst_file}
+fi
+
+ln -s ${src_file} ${dst_file}
