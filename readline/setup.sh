f00f6beb3ba178ef52f425c0195ad39adf6e9dca
vagrant <vagrant@osx-10_9.vagrantup.com>
2015-04-18 19:48:03 +0900

    implement readline setup script.

--- readline/setup.sh
+++ readline/setup.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-echo "sorry, not implemented."
+src_file=$(cd `dirname $01`;pwd)/_inputrc
+dst_file=~/.inputrc
+
+if [ -e ${dst_file} ]; then
+    rm ${dst_file}
+fi
+
+ln -s ${src_file} ${dst_file}
